78b9a7ae5ded167a5d60228a247c3028e81561f4
Enyioma Nwadibia <enyiomanwadibia@Enyiomas-MacBook-Pro.local>
2018-11-08 14:42:03 +0100

    Updates

---
+++ precache-manifest.5c5ad8a2f6a5759e613dc04d2844d7c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e697098838d1e486103d",
+    "url": "/tic-tac-toe-app/static/css/main.c81bf1c2.chunk.css"
+  },
+  {
+    "revision": "e697098838d1e486103d",
+    "url": "/tic-tac-toe-app/static/js/main.e6970988.chunk.js"
+  },
+  {
+    "revision": "8391c07e1621390095e6",
+    "url": "/tic-tac-toe-app/static/js/1.8391c07e.chunk.js"
+  },
+  {
+    "revision": "483aae2a5bb2f441778a",
+    "url": "/tic-tac-toe-app/static/js/runtime~main.483aae2a.js"
+  },
+  {
+    "revision": "a2b9caf022cdda3119a18aba8540722c",
+    "url": "/tic-tac-toe-app/index.html"
+  }
+];
